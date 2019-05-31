2672bbbce511be971c9e948c667ec5de17135b66
Henry T <henryt.inc@gmail.com>
2019-05-30 21:51:44 -0400

    Remote server deployment

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 echo "Starting deployment"
 
-scp -i ~/.ssh/id_rsa * pi@192.168.0.100:/home/pi/SPARC-FMS
+scp -i ~/.ssh/id_rsa -r SERVER/* pi@192.168.0.100:/home/pi/SPARC-FMS/SERVER
 
 echo "Files copied"
 
 echo "Restarting server..."
 
-ssh pi@192.168.0.100 'tmux kill-server; sudo pkill python3; tmux new-session -d "cd SERVER; sudo python3 server.py; read;"'
+ssh pi@192.168.0.100 'tmux kill-server; sudo pkill python3; tmux new-session -d "cd SPARC-FMS/SERVER; sudo python3 server.py; read;"'
 
 echo "Done."
